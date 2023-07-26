c73f8a83f637a1804e202a1a57be931be6f205d3
2021BTech <daffydanni@gmail.com>
2023-07-26 16:28:51 +0100

    Pages & React Router Dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// components
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgetPassword from "./pages/ForgetPassword";
+import Offers from "./pages/Offers";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline"> Let's begin </h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/signIn" element={<SignIn />} />
+          <Route path="/signUp" element={<SignUp />} />
+          <Route path="/forgetPassword" element={<ForgetPassword />} />
+          <Route path="/offers" element={<Offers />} />
+        </Routes>
+      </Router>
     </>
   );
 }
